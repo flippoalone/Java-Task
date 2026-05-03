7484a6e31c13e8d3f851cc2423941b0a14c81593
Akash <flippoalone@gmail.com>
2026-05-03 17:37:32 +0530

    Update

--- FetchUser/src/main/java/com/example/demo/UserController.java
+++ FetchUser/src/main/java/com/example/demo/UserController.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,10 +9,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/user")
 public class UserController {
+//	injects UserService here
+	@Autowired
+    private UserService userService; 
 	
 	 @GetMapping("/{id}")
 	    public String getUserById(@PathVariable int id) {
 	        // Simulating a user fetch
 	        return "User fetched with ID: " + id;
+	    }
+	 
+	 @GetMapping("/all")
+	    public String getAllUsers() {
+	        return userService.getAllUsers();
 	    }
 }
